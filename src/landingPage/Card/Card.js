dc479e22d9514376db660981b536eaa52f5fe442
veronika-simic <veronikasimic21@gmail.com>
2022-11-25 20:46:13 +0100

    position banner above image cards

--- src/landingPage/Card/Card.js
+++ src/landingPage/Card/Card.js
@@ -8,17 +8,19 @@ import "../Banner/Banner.css";
 
 export default function Card({ countries }) {
   return (
-    <div className="cards">
+    <>
       <Banner></Banner>
-      {countries.length > 0 &&
-        countries.map((c, index) => (
-          <div key={index} className={`card card-${index + 1}`}>
-            <NavLink to={`/${c.name}`} style={{ textDecoration: "none" }}>
-              <h3>{c.name}</h3>
-              <p className="hidden">Belgrade is the capital of Serbia.</p>
-            </NavLink>
-          </div>
-        ))}
-    </div>
+      <div className="cards">
+        {countries.length > 0 &&
+          countries.map((c, index) => (
+            <div key={index} className={`card card-${index + 1}`}>
+              <NavLink to={`/${c.name}`} style={{ textDecoration: "none" }}>
+                <h3>{c.name}</h3>
+                <p className="hidden">Belgrade is the capital of Serbia.</p>
+              </NavLink>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
