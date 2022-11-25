d4227ac0c4e62b3f05a2069bb7c20baef7fff2d9
Bruno Perkovic <brunko10@gmail.com>
2022-11-25 21:08:52 +0100

    smh

--- src/Pages/CountryPage.js
+++ src/Pages/CountryPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function CountryPage({country}) {
-    return (
-        <div>
-          <NavLink to={"/"}>
-            <button>Return To Homepage</button>
-          </NavLink>
-         {country.name}
-        </div>
-      )
+function CountryPage({ country }) {
+  return (
+    <div>
+      <NavLink to={"/"}>
+        <button>Return To Homepage</button>
+      </NavLink>
+      {country.name}
+    </div>
+  );
 }
 
 export default CountryPage;
--- src/landingPage/Card/Card.js
+++ src/landingPage/Card/Card.js
@@ -1,48 +1,7 @@
 import * as React from "react";
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-import "./Card.css";
-import { NavLink } from "react-router-dom";
-import { getAllCountries } from "../../api/api";
-import Banner from "../Banner/Banner";
-import "../Banner/Banner.css";
-import { Key } from "@mui/icons-material";
-/* import CroatiaPage from "../Pages/CroatiaPage";
-import SerbiaPage from "../Pages/SerbiaPage";
-import MexicoPage from "../Pages/MexicoPage";
- */
-
-export default function Card({ locationData, setLocationData }) {
-  const [country, setCountry] = useState([]);
-  useEffect(() => {
-    const getAllCountriesFromApi = async () => {
-      const response = await getAllCountries();
-      setCountry(response.data);
-      setLocationData(response.data);
-    };
-    getAllCountriesFromApi();
-  }, []);
-
-  console.log(country);
-
-  function checkWhichCity(city) {
-    if (city.name === "Beograd") {
-      return <p className="hidden">{city.name} is the capital of Serbia</p>;
-    } else if (city.name === "Zagreb") {
-      return <p className="hidden">{city.name} is the capital of Croatia</p>;
-    } else {
-      return (
-        <p className="hidden">
-          {city.name} is one of the biggest cities in Mexico
-        </p>
-      );
-    }
-  }
-=======
 
 import "./Card.css";
 import { NavLink } from "react-router-dom";
->>>>>>> dc479e22d9514376db660981b536eaa52f5fe442
 
 import Banner from "../Banner/Banner";
 import "../Banner/Banner.css";
