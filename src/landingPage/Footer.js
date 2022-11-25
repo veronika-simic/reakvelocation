eb419b08507b86af4a8ab242db4c53906d3e8931
veronika-simic <veronikasimic21@gmail.com>
2022-11-25 20:15:42 +0100

    fix footer

--- src/landingPage/Footer.js
+++ src/landingPage/Footer.js
@@ -14,12 +14,10 @@ export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0)
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box>
       <BottomNavigation
         sx={{
-          position: "fixed",
-          bottom: 0,
-          width: 1.0,
+          width: '100%',
           height: 70,
           bgcolor: "warning.main",
         }}
