3a8897d5599ef29e87f322d2e832c2642c818821
Bruno Perkovic <brunko10@gmail.com>
2022-11-25 12:58:38 +0100

    added not found page route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Homepage from "./Pages/Homepage";
 import CroatiaPage from "./Pages/CroatiaPage";
 import SerbiaPage from "./Pages/SerbiaPage";
 import MexicoPage from "./Pages/MexicoPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/Croatia" element={<CroatiaPage />} />
         <Route path="/Mexico" element={<MexicoPage />} />
         <Route path="/Serbia" element={<SerbiaPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <NavLink to={"/"}>
+        <button> Homepage</button>
+      </NavLink>
+      <div>Not Found</div>
+
+      <div>404</div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
