639d5c05231be291469ab24a031ce1a9c1b80a65
Bruno Perkovic <brunko10@gmail.com>
2022-11-25 15:54:05 +0100

    mock podaci

--- src/Pages/MexicoPage.js
+++ src/Pages/MexicoPage.js
@@ -3,19 +3,20 @@ import { NavLink } from "react-router-dom";
 import "./MexicoPage.css";
 
 const MexicoPage = () => {
+  const data = {
+    id: 1,
+    name: "Guadalajara",
+    title: "Food & Drinks",
+    picture: "image",
+    description:
+      " Croatian cuisine is heterogeneous and is known as a cuisine of the regions, since every region of Croatia has its own distinct culinary tradition. Its roots date back to ancient times. The differences in the selection of foodstuffs and forms of cooking are most notable between those in mainland and those in coastal regions. Mainland cuisine is more characterized by the earlier Slavic and the more recent contacts with Hungarian and Turkish cuisine, using lard for cooking, and spices such as black pepper, paprika, and garlic.",
+  };
   return (
     <div className="mexico">
-      <div className="mexicoHeader">
-        <NavLink to={"/"}>
-          <button> Return to homepage</button>
-        </NavLink>
-
-        <div>Guadalajara</div>
+      <div className="mexicoHeader">{data.name}</div>
+      <div className="categories">
+        <div className="foodBeverage">{data.title}</div>
       </div>
-
-      <div className="categories"></div>
-
-      <div className="footer"></div>
     </div>
   );
 };
--- src/landingPage/Card.js
+++ src/landingPage/Card.js
@@ -1,10 +1,6 @@
 import * as React from "react";
 import "./Card.css";
 import { NavLink } from "react-router-dom";
-/* import CroatiaPage from "../Pages/CroatiaPage";
-import SerbiaPage from "../Pages/SerbiaPage";
-import MexicoPage from "../Pages/MexicoPage";
- */
 
 export default function Card() {
   return (
