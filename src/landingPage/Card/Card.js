5eb868439317fee978742d4ee4a324101bbd7282
Bruno Perkovic <brunko10@gmail.com>
2022-11-26 10:06:22 +0100

    responsive image description on card comp

--- src/landingPage/Card/Card.js
+++ src/landingPage/Card/Card.js
@@ -1,12 +1,25 @@
-import * as React from "react"
+import * as React from "react";
 
-import "./Card.css"
-import { NavLink } from "react-router-dom"
+import "./Card.css";
+import { NavLink } from "react-router-dom";
 
-import Banner from "../Banner/Banner"
-import "../Banner/Banner.css"
+import Banner from "../Banner/Banner";
+import "../Banner/Banner.css";
 
 export default function Card({ countries }) {
+  const checkWhichCountry = (c) => {
+    if (c.name === "Beograd") {
+      return <p className="hidden">{c.name} is the capital of Serbia.</p>;
+    } else if (c.name === "Zagreb") {
+      return <p className="hidden">{c.name} is the capital of Croatia.</p>;
+    } else {
+      return (
+        <p className="hidden">
+          {c.name} is the one ofthe largest cities in Mexico.
+        </p>
+      );
+    }
+  };
   return (
     <>
       <Banner></Banner>
@@ -16,11 +29,11 @@ export default function Card({ countries }) {
             <div key={index} className={`card card-${index + 1}`}>
               <NavLink to={`/${c.name}`} style={{ textDecoration: "none" }}>
                 <h3>{c.name}</h3>
-                <p className="hidden">Belgrade is the capital of Serbia.</p>
+                {checkWhichCountry(c)}
               </NavLink>
             </div>
           ))}
       </div>
     </>
-  )
+  );
 }
