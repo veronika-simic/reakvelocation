5c2e605283ea4394e52f3bafbb0c3dd6eeeb1782
Bruno Perkovic <brunko10@gmail.com>
2022-11-26 08:25:57 +0100

    maping doesnt work, try to fix it

--- src/Pages/CountryPage.js
+++ src/Pages/CountryPage.js
@@ -1,14 +1,56 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function CountryPage({ country }) {
+function CountryPage({ country, category }) {
+  function checkWhichCountry(cat, country) {
+    console.log(cat.name);
+    console.log(country.name);
+
+    console.log(country.name.includes(cat.name));
+    if (country.name.includes(cat.name)) {
+      return (
+        <div>
+          <div className="countryName">{country.name}</div>
+          <div className="countryDescription">{country.description}</div>
+
+          <div className="imageRow">
+            <div className="image">
+              <img
+                src={`${cat.picture}`}
+                alt="This is an alt to remove linter"
+              />
+            </div>
+            <div className="imageDescription">{cat.description}</div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  console.log(country);
   return (
-    <div>
-      <NavLink to={"/"}>
-        <button>Return To Homepage</button>
-      </NavLink>
-      {country.name}
-    </div>
+    <>
+      {category
+        .filter((cat) => country.name.includes(cat.name))
+        .map((c) => {
+          return (
+            <div>
+              <div className="countryName">{country.name}</div>
+              <div className="countryDescription">{country.description}</div>
+
+              <div className="imageRow">
+                <div className="image">
+                  <img
+                    src={`${c.picture}`}
+                    alt="This is an alt to remove linter"
+                  />
+                </div>
+                <div className="imageDescription">{c.description}</div>
+              </div>
+            </div>
+          );
+        })}
+    </>
   );
 }
 
--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,12 @@
-import { createAxionsInstance } from './apiHelper';
-const axiosInstance = createAxionsInstance(
-    'http://localhost:3003/'
-);
+import { createAxionsInstance } from "./apiHelper";
+const axiosInstance = createAxionsInstance("http://localhost:3003/");
 
 export const getAllCountries = async () => {
-    const response = await axiosInstance.get('/countries');
-    return response;
+  const response = await axiosInstance.get("/countries");
+  return response;
+};
+
+export const getAllCategories = async () => {
+  const response = await axiosInstance.get("/categories");
+  return response;
 };
