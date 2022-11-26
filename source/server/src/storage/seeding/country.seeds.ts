c41a58d79b1f8b56e1cdc71c5079d05df196d355
veronika-simic <veronikasimic21@gmail.com>
2022-11-26 09:52:28 +0100

    fix backend, add image

--- source/server/src/storage/seeding/country.seeds.ts
+++ source/server/src/storage/seeding/country.seeds.ts
@@ -6,18 +6,18 @@ export const initialCountries: CountryOrmEntity[] = [
     id: 1,
     name: 'Beograd',
     description: 'Belgrade is one of the oldest continuously inhabited cities in Europe and the world. One of the most important prehistoric cultures of Europe, the Vinča culture, evolved within the Belgrade area in the 6th millennium BC. In antiquity, Thraco-Dacians inhabited the region and, after 279 BC, Celts settled the city, naming it Singidūn',
-    picture:'slika'
+    picture: "https://images.unsplash.com/photo-1613601740367-410ae03b2ec7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   },
   {
     id: 2,
     name: 'Zagreb',
-    description: 'Zagreb is the capital and largest city of Croatia.[10] It is in the northwest of the country, along the Sava river, at the southern slopes of the Medvednica mountain. Zagreb stands near the international border between Croatia and Slovenia at an elevation of approximately 122 m (400 ft) above sea level.[11] At the 2021 census, the city had a population of 767,131.[12] The population of the Zagreb urban agglomeration is 1,071,150,[2] approximately a quarter of the total population of Croatia.',
-    picture:'slika'
+    description: 'Zagreb is the capital and largest city of Croatia. It is in the northwest of the country, along the Sava river, at the southern slopes of the Medvednica mountain. Zagreb stands near the international border between Croatia and Slovenia at an elevation of approximately 122 m (400 ft) above sea level. At the 2021 census, the city had a population of 767,131. The population of the Zagreb urban agglomeration is 1,071,150,approximately a quarter of the total population of Croatia.',
+    picture: "https://images.unsplash.com/photo-1599624893198-d000bdd8bef8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   },
     {
     id: 3,
-    name: 'Guadaljararar',
-    description: 'Guadalajara is a metropolis in western Mexico and the capital of the state of Jalisco. According to the 2020 census, the city has a population of 1,385,629 people, while the Guadalajara metropolitan area has a population of 5,268,642 people, making it the third-largest metropolitan area in the country and the twentieth largest metropolitan area in the America Guadalajara has the second-highest population density in Mexico, with over 10,361 people per square kilometer.[',
-    picture:'slika'
+    name: 'Guadalajara',
+    description: 'Guadalajara is a metropolis in western Mexico and the capital of the state of Jalisco. According to the 2020 census, the city has a population of 1,385,629 people, while the Guadalajara metropolitan area has a population of 5,268,642 people, making it the third-largest metropolitan area in the country and the twentieth largest metropolitan area in the America Guadalajara has the second-highest population density in Mexico, with over 10,361 people per square kilometer',
+    picture:"https://images.unsplash.com/photo-1582456032629-4a54a1a65697?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1832&q=80"
   },
 ];
